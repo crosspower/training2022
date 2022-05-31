9ed4427eb2ff6dd9697815bdddccea6b7c3c3ff0
eiki-ogawa <eiki-ogawa@x-power.co.jp>
2022-05-31 16:32:49 +0900

    TRAINING2022-27 【テスト】【ログイン機能】　空白、文字数越えの際にエラーメッセージを追加

--- AttendanceManager/src/main/java/com/example/demo/service/LoginServiceImpl.java
+++ AttendanceManager/src/main/java/com/example/demo/service/LoginServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import java.util.Locale;
 import java.util.Optional;
 
+import javax.validation.ValidationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,12 +44,30 @@ public class LoginServiceImpl implements AuthenticationProvider {
 		String code = (String) auth.getPrincipal();
 		String password = (String) auth.getCredentials();
 		Optional<Login> loginOpt = null;
+		
+		// エラーメッセージ
 		String failure_message = messagesource.getMessage("E0003", null, Locale.JAPAN);
 		String null_message = messagesource.getMessage("E0004", null, Locale.JAPAN);
+		// IDとパスワードの文字数を両方超えた場合
+		//　「文字数に失敗しました」と一度エラーを発生させる。
+		String over_message = messagesource.getMessage("E0002", new String[]{"文字数"}, Locale.JAPAN);
+		String over_id_message = messagesource.getMessage("E0005", new String[]{"ID", "10"}, Locale.JAPAN);
+		String over_pass_message = messagesource.getMessage("E0005", new String[]{"パスワード", "20"}, Locale.JAPAN);
 		
 		// 空白の場合
 		if("".equals(code) || "".equals(password)) {
 			throw new AuthenticationCredentialsNotFoundException(null_message);
+		}
+		//　文字数制限の場合
+		if (code.length() > 10) {
+			if (password.length() > 20) {
+				throw new AuthenticationCredentialsNotFoundException(over_message);
+			} else {
+				throw new AuthenticationCredentialsNotFoundException(over_id_message);
+			}
+		}
+		if (password.length() > 20) {
+			throw new AuthenticationCredentialsNotFoundException(over_pass_message);	
 		}
 		
 		//　データベースで照合
