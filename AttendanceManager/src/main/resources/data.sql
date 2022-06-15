11da7046514f4dbd12a61a51585e0beb50022351
github ryo10929174 <ryo-abiko@x-power.co.jp>
2022-06-15 17:23:59 +0900

    20220615

--- AttendanceManager/src/main/resources/data.sql
+++ AttendanceManager/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO employees(code, name, role, password)
-VALUES ('test_code', 'test_name', 1, 'test_pass');
+VALUES ('test_code', 'test_name', 1, '$2a$10$nBCCwX10JR0sIEPxznPksef.Y8a7UtqVNjgCCPuJzoU00yicWTX1y');
